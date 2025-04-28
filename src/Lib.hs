6c23eb08e94bf5b7dcb56b97e1a2f1ccb88dd2f7
amancayam <amagronearancibia@frba.utn.edu.ar>
2025-04-28 12:27:56 -0300

    Agrego Personajes

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,31 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+data Personaje = UnPersonaje {
+nombre :: String,
+poderbasico :: Personaje -> Personaje,
+podersuper :: String,
+superPoderActivo :: Bool, 
+cantidadDeVida :: Int,
+}
+
+espina :: Personaje
+espina = UnPersonaje {
+    nombre = "Espina",
+    poderbasico = bolaDeEspina,
+    superPoderActivo = undefined,
+    superPoderActivo = True,
+    cantidadDeVida = 4800,
+}
+pamela :: Personaje
+pamela = UnPersonaje {
+    nombre = "Pamela",
+    poderbasico = lluviaDeTuercas,
+    superPoderActivo = undefined,
+    superPoderActivo = False,
+    cantidadDeVida = 9600,
+}
+
+bolaDeEspina :: Personaje -> Personaje
+ 
